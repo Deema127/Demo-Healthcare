d6bc7309d9642261a978a77ed3592b93c75e2dfc
Deema <Deema@DESKTOP-BD20QK2>
2026-06-06 15:07:56 +0300

    new files

---
+++ Automationweb/src/test/java/day04/LearnAlert.java
@@ -0,0 +1,34 @@
+package day04;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LearnAlert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://demo.automationtesting.in/Alerts.html");
+		driver.manage().window().maximize();
+//		simple Alert
+//		driver.findElement(By.id("OKTab")).click();
+//		String text = driver.switchTo().alert().getText();
+//		System.out.print(text);
+//		driver.switchTo().alert().accept();
+
+//		confirmation Alert
+//		driver.findElement(By.partialLinkText("Alert with OK & Cancel")).click();
+//		driver.findElement(By.id("CancelTab")).click();
+//		driver.switchTo().alert().dismiss();
+		
+//		promt Alert
+		driver.findElement(By.partialLinkText("Alert with Textbox")).click();
+		driver.findElement(By.id("Textbox")).click();
+		driver.switchTo().alert().sendKeys("deem");
+		driver.switchTo().alert().accept();
+		
+		
+
+	}
+
+}
---
+++ Automationweb/src/test/java/day04/LearnXpath.java
@@ -0,0 +1,21 @@
+package day04;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LearnXpath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://katalon-demo-cura.herokuapp.com/");
+		driver .manage().window().maximize();
+//		driver.findElement(By.xpath("//a[@id='btn-make-appointment']")).click();
+//		driver.findElement(By.xpath("//a[text()='Make Appointment']")).click();
+//		driver.findElement(By.xpath("//a[contains(@id,'btn-make')]")).click();
+//		driver.findElement(By.xpath("//a[contains(text(),'Make')]")).click();
+		driver.findElement(By.xpath("//a[starts-with(@id,'btn-make')]")).click();
+
+	}
+
+}
